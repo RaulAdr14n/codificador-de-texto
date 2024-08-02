966797a998628bc2a53e4afded68ae021f4341f0
Raulete <raulahernandez9@gmail.com>
2024-08-02 20:55:06 -0300

    Agregar la logica de encriptado, desencriptado y copiado

---
+++ app.js
@@ -0,0 +1,98 @@
+let textoInput = document.getElementsByClassName('encriptador__texto')[0];
+let botonEncriptar = document.getElementsByClassName('encriptador__opciones__encriptar')[0];
+let botonDesencriptar = document.getElementsByClassName('encriptador__opciones__desencriptar')[0];
+let textoOutput = document.getElementsByClassName('resultado__texto')[0];
+let botonCopiar = document.getElementsByClassName('resultado__copiar')[0];
+
+botonEncriptar.addEventListener('click', encriptar);
+botonDesencriptar.addEventListener('click', desencriptar);
+botonCopiar.addEventListener('click', copiar);
+textoInput.addEventListener('input', verificarTexto);
+
+function verificarTexto() {
+    if (textoInput.value.trim() !== "") {
+        botonEncriptar.disabled = false;
+        botonDesencriptar.disabled = false;
+    } else {
+        botonEncriptar.disabled = true;
+        botonDesencriptar.disabled = true;
+    }
+}
+function desencriptar(){
+    let textoDesencriptado = "";
+    let texto = textoInput.value;
+    let i = 0;
+    
+    while (i < texto.length) {
+        if (texto.substring(i, i + 5) === "enter") {
+            textoDesencriptado += "e";
+            i += 5;
+        } else if (texto.substring(i, i + 4) === "imes") {
+            textoDesencriptado += "i";
+            i += 4;
+        } else if (texto.substring(i, i + 2) === "ai") {
+            textoDesencriptado += "a";
+            i += 2;
+        } else if (texto.substring(i, i + 4) === "ober") {
+            textoDesencriptado += "o";
+            i += 4;
+        } else if (texto.substring(i, i + 4) === "ufat") {
+            textoDesencriptado += "u";
+            i += 4;
+        } else {
+            textoDesencriptado += texto[i];
+            i++;
+        }
+    }
+    textoOutput.value = textoDesencriptado;
+}
+function encriptar(){
+    let textoEncriptado = "";
+    let cantDeCaracteres = textoInput.value.length;
+     
+     let caracteresNoPermitidos = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "á", "é", "í", "ó", "ú", "ü", "ñ", "à", "è", "ì", "ò", "ù", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "=", "{", "}", "[", "]", "|", "\\", ":", ";", "\"", "'", "<", ">", ",", ".", "?", "/", "!", "`", "~", "±", "÷", "×", "°", "≤", "≥", "∑", "∆", "∫", "©", "®", "™", "€", "£", "¥", "§", "¶", "\t", "&nbsp;"];
+
+     
+     for (let i = 0; i < cantDeCaracteres; i++) {
+        console.log(textoInput.value[i])
+         if (caracteresNoPermitidos.includes(textoInput.value[i])) {
+             alert("El texto contiene mayúsculas, caracteres especiales o acentos. Solo se permiten letras minúsculas y espacios.");
+             return;
+         }
+     }
+    for(let i=0;i<cantDeCaracteres;i++){
+        switch(textoInput.value[i]){
+            case "e":
+                textoEncriptado += "enter";
+            break;
+            case "i":
+                textoEncriptado += "imes";
+            break;
+            case "a":
+                textoEncriptado += "ai";
+            break;
+            case "o":
+                textoEncriptado += "ober";
+            break;
+            case "u":
+                textoEncriptado += "ufat";
+            break;
+            default:
+                textoEncriptado += textoInput.value[i];
+            break;
+        }
+    }
+    textoOutput.value = textoEncriptado;
+    botonCopiar.disabled = false;
+}
+function copiar(){
+    navigator.clipboard.writeText(textoOutput.value);
+    mostrarPopup();
+}
+function mostrarPopup() {
+    let popup = document.getElementById("notificacion");
+    popup.classList.add("mostrar");
+    setTimeout(() => {
+        popup.classList.remove("mostrar");
+    }, 2000);
+}
